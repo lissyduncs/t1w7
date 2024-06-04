bb352585f6df556abec220cecf241f8e5bd6cadd
Lissy Duncan <duncanlissy@gmail.com>
2024-06-04 21:03:38 +1000

    programs with inputs

---
+++ t1-w6-intro-into-python/area_of_square.py
@@ -0,0 +1,7 @@
+#get the length of the user
+length = float(input("Enter length of the square:"))
+#print(length)
+# calculate and store the area in a variable, area = length * length
+area = length * length
+#print the calculated area to the terminal
+print(f"The calculated area is {area}")
---
+++ t1-w6-intro-into-python/create_email_form.py
@@ -0,0 +1,8 @@
+#Lissy Duncan Coder Academy --> duncanlissy@gmail.com
+first_name = input("Please enter yout first name:")
+last_name = input("Please enter yout last name:")
+company_name = input("Please enter yout company name(no blank spaces):")
+print(f"{first_name}. {last_name}@{company_name}.com")
+
+email = f"{first_name}.{last_name}@{company_name}.com".lower()
+print(email)
