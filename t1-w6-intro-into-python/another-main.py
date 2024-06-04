dd6a6635cf22b7626847837ae06bf87181bc114c
Lissy Duncan <duncanlissy@gmail.com>
2024-06-04 20:47:13 +1000

    new stuff

---
+++ t1-w6-intro-into-python/another-main.py
@@ -0,0 +1,23 @@
+#variables
+#snake_case for python variables
+#camelCase for JavaScript 
+#PascalCase for classes in all languages
+#kebab-case
+
+num1 = 5
+num2 = 7
+
+print(num1 + num2)
+num1 = num 2
+print (num1 + num2)
+
+num3 = 5
+num3 = num3 + 2
+print(num3)
+
+num 3+=3 #num3 = num3 +3
+print(num3)
+total = num1 + num2 + num3
+#string interpolation
+print("the total addition is", total)
+print(f"the total addition is {total}"")
--- t1-w6-intro-into-python/main.py
+++ t1-w6-intro-into-python/main.py
@@ -1,3 +1,64 @@
-print("Hello World")
-print (5-3)
-print ("don't forget to commit")
+#print("Hello World")
+#print (5-3)
+#print ("don't forget to commit")
+
+# print("it's a comment, it wont be printed")
+
+# data types
+# integers
+#4
+#7
+#print(9)
+#print("9")
+#print (type(9))
+#print (type("9"))
+
+#floats
+#2.5
+#5.1245
+#print (3.1415)
+#print (type (-5.63))
+#print (type (-5.))
+
+#complex (imaginary numbers) no worries, we dont cover it
+#2+3j
+#print (type (4+7j))
+
+#operators + - * / %
+#print(4*5)
+#print(15/3)
+#print(15/2)
+#print(7/2)
+
+#string
+#"Hello World"
+#'Hello World'
+#print ("this is a string")
+
+#string concatenating, joi
+#print ("hello" + "world")
+#print (3+5)
+#print ("3" + "5")
+
+# boolean
+#True
+#False
+#print (type(False))
+#0 # falsy value
+#"" #falsy value
+#print (bool(0))
+#print (bool(""))
+#print (bool(-123))
+#print (bool("False"))
+
+#comparison operators <, >, <=, >=, ==, !=
+print (2<3)
+print (5>=5)
+print ("HELLO", lower())
+print ("Hello"=="hello")
+print ("Hello".lower()=="hello")
+
+#logical operators and, or, not
+print (True and False) #AND returns true when both operators are true
+print (True or False)#Or returns True when one or both operators are true
+print (not False) #returns
